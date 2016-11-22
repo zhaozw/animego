45d8a2f8a6044289cf0ced6bc5f1596f5a9454ae
Chaoran Li <superlcr@me.com>
2016-11-23 06:36:09 +0800

    Initial Commit

---
+++ AnimeGo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimeGo
+//
+//  Created by Chaoran Li on 2016/11/23.
+//  Copyright © 2016年 Chaoran Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimeGo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimeGo
+//
+//  Created by Chaoran Li on 2016/11/23.
+//  Copyright © 2016年 Chaoran Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
